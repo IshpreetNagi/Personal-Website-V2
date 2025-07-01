b69418808acd361862855c84a88a2880513c1fcf
Ishpreet Nagi <ishpreetnagi@gmail.com>
2025-07-01 16:34:32 -0400

    Format fixes + Picture loading

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
+
 const SocialIcons = () => (
   <div className=" text-white flex flex-col gap-7 items-center">
     <a href="https://github.com/IshpreetNagi" target="_blank" rel="noopener noreferrer" aria-label="GitHub" className="underline-animation underline-animation-icon">
@@ -17,17 +18,15 @@ const SocialIcons = () => (
 
 const Sidebar: FC = () => {
   return (
-    <div>
-      <nav className="fixed bottom-14 left-14 z-10 bg-white/5 backdrop-blur-sm border-r border-white/5 rounded-3xl w-16 drop-shadow-md text-white text-lg">
+    <div className="flex flex-col items-center fixed bottom-16 left-4 z-50 gap-24">
+      <div className="-rotate-90 text-white/60 text-3xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Ishpreet Nagi</div>
+      <nav className=" bg-white/5 backdrop-blur-sm border-r border-white/5 rounded-3xl w-16 drop-shadow-md text-white text-lg">
         <ul className="list-none m-0 py-9">
           <li>
             <SocialIcons />
           </li>
         </ul>
       </nav>
-      <div className="-rotate-90 fixed bottom-94 -left-0.5 z-50 text-white/60 text-3xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-        Ishpreet Nagi
-      </div>"
     </div>
   );
 };
