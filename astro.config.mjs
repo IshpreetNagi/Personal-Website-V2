c8cbbd0c05f9bdca38be5e83404f98fe9a4301ae
Ishpreet Nagi <ishpreetnagi@gmail.com>
2025-11-19 16:55:31 -0500

    Revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import vercelServerless from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/server";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercelServerless(),
+  output: "server",
+  adapter: vercel(),
 });
